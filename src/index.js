4650e15aa5ec7fa52a65bbad9fa62e34ea679b34
devalibello <alibelloimoukhuede@gmail.com>
2023-12-16 21:16:08 +0300

    Fix react-redux error

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
-import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 import App from './App';
 
 const store = configureStore({
   reducer: rootReducer,
-  middleware: [thunk],
 });
 
 ReactDOM.createRoot(
